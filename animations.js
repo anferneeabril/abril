c34aaa8b8d3542e3ac2b392cf72d84c0446eb643
Anfernee Abril <43106011+anferneeabril@users.noreply.github.com>
2020-01-28 13:46:57 -0400

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,11 @@
+// // import TweenMax from "gsap/TweenMax"
+// // import Draggable from "gsap/Draggable"
+
+
+const move = TweenMax.to("img.cen", 3, {x:150, delay:1, ease: Power2.easeOut});
+
+TweenMax.to("div.moveDescriptrion", 3, {x:-250, delay:1, ease: Power2.easeOut });
+
+// // TweenMax.to("div.move12", 3, {x:-250, delay:1, ease: Power2.easeOut });
+
+// // TweenMax.to("div.move13", 3, {x:250, delay:1, ease: Power2.easeOut });
